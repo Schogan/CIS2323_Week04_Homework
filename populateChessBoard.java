3611da35363357d6dd7bbee1460b5f326416daf3
Schogan <navy15@gmail.com>
2018-09-17 14:09:33 -0500

    testing

--- populateChessBoard.java
+++ populateChessBoard.java
@@ -9,12 +9,14 @@ public class populateChessBoard{
 		
 		int count=0;
 		String type;
+		int posX=0;
+		int posY=0;
 		
 		while (count<COUNT){
 			for(int i=1;i<3;i++){
-				for(int j=1;j<17;j++){
+				for(int j=1;j<9;j++){
 					if(i=2){
-						type = "pawn"; 
+						type = "pawn";
 					}
 					if(i=1 && (j=1||j=8)){
 						type = "rook";
@@ -33,11 +35,47 @@ public class populateChessBoard{
 					}
 					
 					whitePieces = new chessPiece();
-					whitePieces.createPiece("White", type);
+					whitePieces.createPiece("White", type, i, j);
+					count++;
+				}
+			}
+			
+			for(int i=8;i>6;i--){
+				for(int j=1;j<9;j++){
+					if(i=7){
+						type = "pawn";
+					}
+					if(i=8 && (j=1||j=8)){
+						type = "rook";
+					}
+					if(i=8 && (j=2||j=7)){
+						type = "knight";
+					}
+					if(i=8 && (j=3||j=6)){
+						type = "bishop";
+					}
+					if(i=8 && j=4){
+						type = "queen";
+					}
+					if(i=8 && j=5){
+						type = "king";
+					}
+					
+					blackPieces = new chessPiece();
+					blackPieces.createPiece("Black", type, i, j);
 					count++;
 				}
 			}
 		}
+		
+		for(int i=0;i<COUNT;i++){
+			System.out.println(whitePieces[i].printInfo());
+		}
+		for(int i=0;i<COUNT;i++){
+			System.out.println(blackPieces[i].printInfo());
+		}
+		
+		/*
 		//Test piece A
 		chessPiece pieceA = new chessPiece();
 		System.out.println("This is the first test piece ");
@@ -60,6 +98,6 @@ public class populateChessBoard{
 		String pieceBstatus = pieceB.printInfo();
 		System.out.println(pieceBstatus);
 		
-		
+	*/	
 	}	
 }
