9a29043e4e99a10edc3c18e31fbcdd44a5731f59
Schogan <navy15@gmail.com>
2018-09-17 14:35:41 -0500

    removed comments

--- populateChessBoard.java
+++ populateChessBoard.java
@@ -77,29 +77,5 @@ public class populateChessBoard{
 			System.out.println(blackPieces[i].printInfo());
 		}
 		
-		/*
-		//Test piece A
-		chessPiece pieceA = new chessPiece();
-		System.out.println("This is the first test piece ");
-		
-		pieceA.setColor("white");
-		pieceA.setPosition(1,1);
-		pieceA.setPieceType("LeftSideRook");
-		
-		String pieceAstatus = pieceA.printInfo();
-		System.out.println(pieceAstatus);
-		
-		//Test piece B
-		chessPiece pieceB = new chessPiece();
-		System.out.println("This is the second test piece ");
-		
-		pieceB.setColor("black");
-		pieceB.setPosition(8,2);
-		pieceB.setPieceType("RightSideBishop");
-		
-		String pieceBstatus = pieceB.printInfo();
-		System.out.println(pieceBstatus);
-		
-	*/	
 	}	
 }
